7c4e8ab30f035baa26f0ed63af3f81c697785292
Yudha Pratama <yudha.webdev@gmail.com>
2018-12-17 12:59:52 +0700

    Penambahan function get list bank virtual account

--- index.js
+++ index.js
@@ -41,3 +41,20 @@ exports.nameValidator = (secretKey, json) => {
       });
   });
 };
+
+exports.getListBankVA = (secretKey) => {
+  return new Promise((resolve, reject) => {
+    const base64 = Buffer.from(`${secretKey}:`).toString('base64');
+    unirest
+      .get('https://api.xendit.co/available_virtual_account_banks')
+      .headers({ Authorization: `Basic ${base64}` })
+      .timeout(30000)
+      .end((response) => {
+        if (response.statusCode !== 200) {
+          reject(response.body);
+        }
+
+        resolve(response.body);
+      });
+  });
+};
