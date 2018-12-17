eb0e450d8ceda973828b141b800dd003360f40b4
Yudha Pratama <yudha.webdev@gmail.com>
2018-12-17 12:07:11 +0700

    Penambahan function check name validator

--- index.js
+++ index.js
@@ -16,3 +16,21 @@ exports.getBalance = (secretKey) => {
       });
   });
 };
+
+exports.nameValidator = (secretKey, json) => {
+  return new Promise((resolve, reject) => {
+    const base64 = Buffer.from(`${secretKey}:`).toString('base64');
+    unirest
+      .post('https://api.xendit.co/bank_account_data_requests')
+      .headers({ Authorization: `Basic ${base64}` })
+      .send({ bank_account_number: json.bank_account_number, bank_code: json.bank_code })
+      .timeout(30000)
+      .end((response) => {
+        if (response.statusCode !== 200) {
+          reject(response.body);
+        }
+
+        resolve(response.body);
+      });
+  });
+};
