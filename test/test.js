a603804385be929c80eccc4810cc04a0af3647f6
Yudha Pratama <yudha.webdev@gmail.com>
2018-12-17 16:42:04 +0700

    Penambahan mocha testing

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 const xendit = require('../index');
 
+const secretKey = '';
+
 describe('Xendit', () => {
   it('Get balance from your xendit account', (done) => {
-    xendit.getBalance('xnd_development_NIiCfL8l0OD9ncdoerQaEjeXZdKmqNl4kyXnRxnWzTbGjDg9zhw', 'HOLDING')
+    xendit.getBalance(secretKey)
       .then((results) => {
         console.log(results);
         done();
@@ -16,9 +18,94 @@ describe('Xendit', () => {
   it('Check name validator', (done) => {
     const json = {
       bank_account_number: '1234567890',
-      bank_code: 'BCA',
+      bank_code: 'BNI',
     };
-    xendit.nameValidator('xnd_development_NIiCfL8l0OD9ncdoerQaEjeXZdKmqNl4kyXnRxnWzTbGjDg9zhw', json)
+    xendit.nameValidator(secretKey, json)
+      .then((results) => {
+        console.log(results);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it('Get list bank virtual account', (done) => {
+    xendit.getListBankVA(secretKey)
+      .then((results) => {
+        console.log(results);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it('Create virtual account', (done) => {
+    const json = {
+      external_id: 'VA_fixed-{{$timestamp}}',
+      bank_code: 'BNI',
+      name: 'Yudha Pratama',
+    };
+    xendit.createVirtualAccount(secretKey, json)
+      .then((results) => {
+        console.log(results);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it('Create retail outlet', (done) => {
+    const json = {
+      external_id: 'FPC-{{$timestamp}}',
+      retail_outlet_name: 'ALFAMART',
+      name: 'Yudha Pratama',
+      expected_amount: 25000,
+    };
+    xendit.createRetailOutlet(secretKey, json)
+      .then((results) => {
+        console.log(results);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it('Create invoice', (done) => {
+    const json = {
+      external_id: 'invoice-{{$timestamp}}',
+      amount: 1800000,
+      payer_email: 'yudha.webdev@gmail.com',
+      description: 'Invoice Demo #123',
+    };
+    xendit.createInvoice(secretKey, json)
+      .then((results) => {
+        console.log(results);
+        done();
+      })
+      .catch((err) => {
+        console.log(err);
+        done();
+      });
+  });
+
+  it('Create disbursement', (done) => {
+    const json = {
+      external_id: 'disb-{{$timestamp}}',
+      amount: 15000,
+      bank_code: 'BCA',
+      account_holder_name: 'Joe',
+      account_number: 1234567890,
+      description: 'Disbursement from xendit-connect',
+    };
+    xendit.createDisbursement(secretKey, json)
       .then((results) => {
         console.log(results);
         done();
